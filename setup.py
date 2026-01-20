2832ec8b535b2b38930f2b7eecc30fafd974ab9c
Nikhil P Marihal <nikhilpmarihal@gmail.com>
2026-01-20 19:10:28 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name ="medical_chatbot",
+    version="0.1.0",
+    author="Nikhil P Mariahl",
+    author_email="nikhilpmarihal@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
